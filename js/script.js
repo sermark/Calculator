0d3aa64b9abe88c49ac815c653e049a0775d84b4
sermark <win295@gmail.com>
2017-05-27 15:49:14 +0300

    через eval

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 let calc = document.getElementById('calculator');
-console.log(calc);
-let fieldValue = document.getElementById('fieldValue');
+let display = document.getElementById('display');
 let res = 0;
-//
 
 calc.onclick = (event) => {
   let target = event.target;
   if (target.tagName != 'BUTTON') return;
-  let val = target.getAttribute('data-value');
-
-  res += val;
-  fieldValue.setAttribute('value', res.slice(1));
-
-  if(target.getAttribute('data-value') == 'C') clear();
+  	if(target.innerHTML !== '=') {
+  		let val = target.innerHTML;
+  		res += val;
+  		display.value = res.substring(1, 9);
+  	}
+  else updateDisplay();
+  if(target.innerHTML == 'C') clear();
 }
 
 let clear = () => {
-  fieldValue.setAttribute('value', 0);
+  display.value = 0;
   res = 0;
+}
+
+let updateDisplay = () => {
+	display.value = eval(display.value);
 }
