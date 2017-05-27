b79b968eb6838aa5ea98652c6c0a306a9e1a7dbf
sermark <win295@gmail.com>
2017-05-27 23:29:30 +0300

    without eval

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 let calc = document.getElementById('calculator');
 let display = document.getElementById('display');
 let res = 0;
+let op;
 
 calc.onclick = (event) => {
   let target = event.target;
   if (target.tagName != 'BUTTON') return;
-  	if(target.innerHTML !== '=') {
+  	if(target.innerHTML !== '=' && target.innerHTML !== 'C') {
+  		if (target.innerHTML == '+' || target.innerHTML == '-' || target.innerHTML == '*' || target.innerHTML == '/'){
+  			op = target.innerHTML;
+  		}
   		let val = target.innerHTML;
   		res += val;
-  		display.value = res.substring(1, 9);
+  		display.value = res.substring(1);
   	}
-  else updateDisplay();
+  	else updateDisplay();
+
   if(target.innerHTML == 'C') clear();
 }
 
@@ -20,5 +25,25 @@ let clear = () => {
 }
 
 let updateDisplay = () => {
-	display.value = eval(display.value);
+	res = display.value.split(op)
+	if (op == '+') display.value = sum(res);
+	else if (op == '-') display.value = diff(res);
+	else if (op == '*') display.value = mul(res);
+	else display.value = div(res);
 }
+
+let sum = (arr) => arr.reduce(function (prev, next){
+	return +prev + +next;
+});
+
+let diff = (arr) => arr.reduce(function (prev, next){
+	return +prev - +next;
+});
+
+let mul = (arr) => arr.reduce(function (prev, next){
+	return +prev * +next;
+});
+
+let div = (arr) => arr.reduce(function (prev, next){
+	return +prev / +next;
+});
