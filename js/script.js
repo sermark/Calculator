8e072c25a803857cb9806d2156b48e5faae49a48
sermark <win295@gmail.com>
2017-06-02 00:36:07 +0300

    wft

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 let calc = document.getElementById('calculator');
 let display = document.getElementById('display');
-<<<<<<< HEAD
-/*let res = '';
-let op;*/
-=======
-let res = '';
-let op;
->>>>>>> origin/master
 
 calc.onclick = (event) => {
   let target = event.target;
   if (target.tagName != 'BUTTON') return;
-<<<<<<< HEAD
     calcApp.clickDigit(target.dataset.value);
 };
 
@@ -86,113 +78,3 @@ let calcApp = {
   }
 };
 
-
-/*calc.onclick = (event) => {
-  let target = event.target;
-  if (target.tagName != 'BUTTON') return;
-    if(target.dataset.value !== '=' && target.dataset.value !== 'C') {
-      if (target.dataset.value == '+' || target.dataset.value == '-' || target.dataset.value == '*' || target.dataset.value == '/'){
-        op = target.dataset.value;
-      }
-      let val = target.dataset.value;
-      res += val;
-      display.value = res;
-    }
-    else calc.updateDisplay();
-=======
-  	if(target.dataset.value !== '=' && target.dataset.value !== 'C') {
-  		if (target.dataset.value == '+' || target.dataset.value == '-' || target.dataset.value == '*' || target.dataset.value == '/'){
-  			op = target.dataset.value;
-  		}
-  		let val = target.dataset.value;
-  		res += val;
-  		display.value = res;
-  	}
-  	else calc.updateDisplay();
->>>>>>> origin/master
-  if(target.dataset.value == 'C') calc.clear();
-}
-
-calc.clear = () => {
-<<<<<<< HEAD
-  display.value = 0;
-=======
-  display.value = '';
->>>>>>> origin/master
-  res = '';
-}
-
-calc.updateDisplay = () => {
-<<<<<<< HEAD
-  res = display.value.split(op);
-  switch (op){
-  case ('+'):
-    display.value = calc.sum(res);
-    res = display.value;
-    break;
-  case ('-'):
-    display.value = calc.diff(res);
-    res = display.value;
-    break;
-  case ('*'):
-    display.value = calc.mul(res);
-    res = display.value;
-    break;
-  case ('/'):
-=======
-	res = display.value.split(op)
-  switch (op){
-	case ('+'):
-    display.value = calc.sum(res);
-    res = display.value;
-    break;
-	case ('-'):
-    display.value = calc.diff(res);
-    res = display.value;
-    break;
-	case ('*'):
-    display.value = calc.mul(res);
-    res = display.value;
-    break;
-	case ('/'):
->>>>>>> origin/master
-    display.value = calc.div(res);
-    res = display.value;
-    break;
-  }
-}
-
-calc.sum = (arr) => arr.reduce(function (prev, next){
-<<<<<<< HEAD
-  return +prev + +next;
-});
-
-calc.diff = (arr) => arr.reduce(function (prev, next){
-  return +prev - +next;
-});
-
-calc.mul = (arr) => arr.reduce(function (prev, next){
-  return +prev * +next;
-});
-
-calc.div = (arr) => arr.reduce(function (prev, next){
-  return +prev / +next;
-});*/
-
-
-=======
-	return +prev + +next;
-});
-
-calc.diff = (arr) => arr.reduce(function (prev, next){
-	return +prev - +next;
-});
-
-calc.mul = (arr) => arr.reduce(function (prev, next){
-	return +prev * +next;
-});
-
-calc.div = (arr) => arr.reduce(function (prev, next){
-	return +prev / +next;
-});
->>>>>>> origin/master
