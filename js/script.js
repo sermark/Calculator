ea5d8b5c2b37a51527056722e3a968f5ebd8077c
sermark <win295@gmail.com>
2017-06-02 00:31:53 +0300

    with obj

--- js/script.js
+++ js/script.js
@@ -1,49 +1,142 @@
 let calc = document.getElementById('calculator');
 let display = document.getElementById('display');
-let res = 0;
-let op;
+/*let res = '';
+let op;*/
 
 calc.onclick = (event) => {
   let target = event.target;
   if (target.tagName != 'BUTTON') return;
-  	if(target.innerHTML !== '=' && target.innerHTML !== 'C') {
-  		if (target.innerHTML == '+' || target.innerHTML == '-' || target.innerHTML == '*' || target.innerHTML == '/'){
-  			op = target.innerHTML;
-  		}
-  		let val = target.innerHTML;
-  		res += val;
-  		display.value = res.substring(1);
-  	}
-  	else updateDisplay();
+    calcApp.clickDigit(target.dataset.value);
+};
 
-  if(target.innerHTML == 'C') clear();
+
+let calcState = {
+  res: '',
+  op: ''
+};
+
+let calcApp = {
+  clickDigit: function (digit) {
+    if(digit != '=' && digit != 'C'){
+       if (digit == '+'|| digit == '-' || digit == '/' || digit == '*') {
+        calcState.op = digit;
+      }
+      calcState.res += digit;
+      display.value = calcState.res;
+    } else calcApp.updateDisplay();
+    if (digit == 'C') calcApp.clear();
+  },
+
+  sum: function (arr){
+    return arr.reduce(function (prev, next) {
+      return +prev + +next;
+    });
+  },
+
+  diff: function (arr){
+    return arr.reduce(function (prev, next) {
+      return +prev - +next;
+    });
+  },
+
+  mul: function (arr){
+    return arr.reduce(function (prev, next) {
+      return +prev * +next;
+    });
+  },
+
+  div: function (arr){
+    return arr.reduce(function (prev, next) {
+      return +prev / +next;
+    });
+  },
+
+  clear: function () {
+    calcState.res = '';
+    calcState.op = '';
+    display.value = '';
+  },
+
+  updateDisplay: function (){
+    console.log(typeof calcState.res);
+    switch (calcState.op){
+    case ('+'):
+      display.value = calcApp.sum(display.value.split(calcState.op));
+      calcState.res = display.value;
+    break;
+    case ('-'):
+      display.value = calcApp.diff(display.value.split(calcState.op));
+      calcState.res = display.value;
+    break;
+    case ('*'):
+      display.value = calcApp.mul(display.value.split(calcState.op));
+      calcState.res = display.value;
+    break;
+    case ('/'):
+      display.value = calcApp.div(display.value.split(calcState.op));
+      calcState.res = display.value;
+    break;
+    }
+  }
+};
+
+
+/*calc.onclick = (event) => {
+  let target = event.target;
+  if (target.tagName != 'BUTTON') return;
+    if(target.dataset.value !== '=' && target.dataset.value !== 'C') {
+      if (target.dataset.value == '+' || target.dataset.value == '-' || target.dataset.value == '*' || target.dataset.value == '/'){
+        op = target.dataset.value;
+      }
+      let val = target.dataset.value;
+      res += val;
+      display.value = res;
+    }
+    else calc.updateDisplay();
+  if(target.dataset.value == 'C') calc.clear();
 }
 
-let clear = () => {
+calc.clear = () => {
   display.value = 0;
-  res = 0;
+  res = '';
 }
 
-let updateDisplay = () => {
-	res = display.value.split(op)
-	if (op == '+') display.value = sum(res);
-	else if (op == '-') display.value = diff(res);
-	else if (op == '*') display.value = mul(res);
-	else display.value = div(res);
+calc.updateDisplay = () => {
+  res = display.value.split(op);
+  switch (op){
+  case ('+'):
+    display.value = calc.sum(res);
+    res = display.value;
+    break;
+  case ('-'):
+    display.value = calc.diff(res);
+    res = display.value;
+    break;
+  case ('*'):
+    display.value = calc.mul(res);
+    res = display.value;
+    break;
+  case ('/'):
+    display.value = calc.div(res);
+    res = display.value;
+    break;
+  }
 }
 
-let sum = (arr) => arr.reduce(function (prev, next){
-	return +prev + +next;
+calc.sum = (arr) => arr.reduce(function (prev, next){
+  return +prev + +next;
 });
 
-let diff = (arr) => arr.reduce(function (prev, next){
-	return +prev - +next;
+calc.diff = (arr) => arr.reduce(function (prev, next){
+  return +prev - +next;
 });
 
-let mul = (arr) => arr.reduce(function (prev, next){
-	return +prev * +next;
+calc.mul = (arr) => arr.reduce(function (prev, next){
+  return +prev * +next;
 });
 
-let div = (arr) => arr.reduce(function (prev, next){
-	return +prev / +next;
-});
+calc.div = (arr) => arr.reduce(function (prev, next){
+  return +prev / +next;
+});*/
+
+
